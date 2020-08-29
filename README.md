e050ffef3fbeacdebb5cb92447e4e93d91d8e625
Lennard Heller <lennard.heller@icloud.com>
2020-08-29 02:00:50 +0200

    added generating ta-data
    + now generating csv data for each asset pair (incl. indicators)

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # StrategyCheck
-just some tool to visualize trading strategy without manually generating images every single time
+
+tool to visualize/check trading strategy so you can use it with your trading bot :]
+
+Currently only for Binance. If you need an account:
+[Register on Binance](https://www.binance.com/en/register?ref=23830900)
+
+## Features
+
+- set conditions to filter the data (conf.py)
+	- minimum quote asset volume per 24h
+	- which quote asset to use (what you pay to buy an asset)
+	- candlestick interval and how many days you want to look back
+	- which assets should be ignored
+- use TA-Lib and custom indicators
+	- edit indicators in marked area in gen_candles.py
+- save all data as csv
+
+## Used Libraries
+
+[pandas](https://github.com/pandas-dev/pandas)
+
+[python-binance](https://github.com/sammchardy/python-binance)
+
+[TA-Lib](https://github.com/mrjbq7/ta-lib)
---
+++ gen_data.py
@@ -0,0 +1,72 @@
+import conf
+from binance.client import Client
+import pandas as pd
+from talib.abstract import *
+
+
+# convert date to seconds and set every relevant non-float column to float
+def only_numlist(candle_elem):
+    return [candle_elem[0] // 1000] + [float(val) if type(val) is str else val for val in candle_elem[1:6]]
+
+
+# generate the classic candles data
+def gen_candles(symbol="BTCUSDT", days=14):
+    c = Client()
+    candles = c.get_historical_klines(
+        symbol,
+        eval(f'c.KLINE_INTERVAL_{conf.candle_interval}'),
+        f'{days} days ago UTC'
+    )
+    candle_data = [only_numlist(candle) for candle in candles]
+    df = pd.DataFrame(candle_data)
+    df.columns = ["date", "open", "high", "low", "close", "volume"]
+    df["date"] = df["date"] / 1000
+    return df
+
+
+def gen_ta_candles(symbol="BTCUSDT", days=14):
+    data = gen_candles(symbol, days)
+    inputs = {
+        'open': data['open'].astype(float),
+        'high': data['high'].astype(float),
+        'low': data['low'].astype(float),
+        'close': data['close'].astype(float),
+        'volume': data['volume'].astype(float),
+    }
+    #################################################################################
+    # set indicators below
+
+    data['close_weight'] = WCLPRICE(inputs)
+    data["middle"] = (data["high"] + data["low"] + data["close"] + data["open"]) / 4
+    data["ema_25"] = EMA(inputs, timeperiod=25)
+    data["wma_50"] = WMA(inputs, timeperiod=50)
+    data["wma_100"] = WMA(inputs, timeperiod=100)
+    data["wma_200"] = WMA(inputs, timeperiod=200)
+
+    # set indicators above
+    #################################################################################
+    data = data.dropna()
+    data.to_csv(f"candle_data/{symbol}_{days}days_{conf.candle_interval}_ta.csv", index=False)
+    print(f'generated {symbol} ta-data')
+
+
+if __name__ == '__main__':
+    client = Client()
+    ticker_df = pd.DataFrame(client.get_ticker())
+
+    # only pairs with desired quote asset
+    filter_ticker_df = ticker_df['symbol'].str[-len(conf.quote_asset):] == conf.quote_asset
+    ticker_df = ticker_df[filter_ticker_df]
+
+    # only pairs with volume > minimal volume of interest to trade
+    filter_ticker_df = ticker_df['quoteVolume'].astype(float) > conf.min_volume
+    ticker_df = ticker_df[filter_ticker_df]
+
+    # only pairs not containing any of the assets to ignore
+    for asset in conf.ignore_assets:
+        filter_ticker_df = ticker_df['symbol'].str.contains(asset)
+        ticker_df = ticker_df[~filter_ticker_df]
+
+    # generate csv data for each pair
+    for pair in ticker_df['symbol'].tolist():
+        gen_ta_candles(pair, conf.days)
